b6913f38a8282109c10778e33aa22efc29633e12
roryholmes <holmes@mpia.de>
2011-02-02 23:14:47 +0000

    euclid: update wrapper
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/euclid@17417 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- ugly_wrapper.py
+++ ugly_wrapper.py
@@ -9,13 +9,14 @@ import matplotlib.pylab as plt
 os.system('rm ./test.txt')
 
 parameter = 'epsilon_max'
-par_range = np.arange(0,1,0.5)
+par_range = np.arange(0,1,0.1)
 
-for i in par_range:
-  command = './master.py %s %lf' % (parameter, par_range[i])
+for i in range(len(par_range)):
+  command = './master.py %s %s' % (parameter, par_range[i])
   print command
   os.system(command)
   time.sleep(0)
+  i+=1
 
 data = np.loadtxt('test.txt')
 
