bafa3fe06f19db9a91abe00f999a5ddf8c2d21c8
roryholmes <holmes@mpia.de>
2011-02-03 01:40:34 +0000

    euclid: parameters now in dictionary
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/euclid@17420 03a3cea6-2c03-0410-ac9b-9271b1c66c29

---
+++ tparameters.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+dic = {
+  'mag_at_ten_sigma'    :     22.5,
+
+  'eta'                 :     0.01,
+  
+  'm_min'               :     19.5,
+  
+  'm_max'               :     22.5,
+  
+  'density_of_stars'    :     50, # all magnitudes
+  
+  'powerlaw'            :     0.25, # B in log10(dN/dm) = A + B*m
+  
+  'FoV'                 :     [0.7, 0.7], # [Δα, Δβ]
+  
+  'ff_samples'          :     [100, 100],
+  
+  'flat_field_order'    :     2,
+  
+  'epsilon_max'         :     1,
+  
+  'sky_limits'          :     [0.0, 10.0, 0.0, 10.0], #[α_min, α_max, β_min, β_max]
+  
+  }
