9800550de121e20ae0edb777ecc1ca6e9d8cc1b0
roryholmes <holmes@mpia.de>
2011-08-01 07:02:54 +0000

    euclid: update plots based on David's comments
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/euclid@19223 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- plot.py
+++ plot.py
@@ -23,6 +23,8 @@ expct_perf = True
 
 alpha = r'Sky Position $\alpha$ (deg$^2$)'
 beta = r'Sky Position $\beta$ (deg$^2$)'
+xlab = r'Focal Plane Position $x$ (deg$^2$)'
+ylab = r'Focal Plane Position $y$ (deg$^2$)'
 
 scale = 2
 fig_width_pt = scale*415.55  # Get this from LaTeX using \showthe\columnwidth
@@ -41,6 +43,7 @@ params = {'backend': 'pdf',
           #'font': {'family':'sans-serif','sans-serif':['Helvetica']}}
 plt.rcParams.update(params)
 plt.rc('font', family = 'serif', serif = 'cmr10')
+
 
 if len(sys.argv) == 2:
   print "Running plotting routine..."
@@ -65,7 +68,7 @@ def plot_flat_fields(map_dic):
   strategy = map_dic['survey']
   print ff_filename
   FoV = params['FoV']
-  plt.rcParams.update({'figure.figsize': [fig_width, fig_width*0.94], 'xtick.labelsize': scale*7, 'ytick.labelsize': scale*7})
+  plt.rcParams.update({'figure.figsize': [fig_width, fig_width*0.95], 'xtick.labelsize': scale*7, 'ytick.labelsize': scale*7})
   fig = plt.figure()
   # Axis for colorbar
   cax = fig.add_axes([0.93, 0.1, 0.03, 0.8])
@@ -91,33 +94,34 @@ def plot_flat_fields(map_dic):
   levels = np.arange(0.5,1.5,0.01)
   CS = plt.contour(god_X,god_Y,god_ff,levels ,colors='k',alpha=0.3)
   CS2 = plt.contour(our_X, our_Y, our_ff, levels,colors='k')
-  plt.ylabel(beta)
+  plt.ylabel(ylab)
   plt.clabel(CS2, inline=1)
   plt.xlim(-FoV[0]/2, FoV[0]/2)
   plt.ylim(-FoV[1]/2, FoV[1]/2)
-
+  #plt.axis('equal')
   plt.gca().set_xticklabels([])
   # Plot residual in god and fitted
   plt.subplot(222)
   a = plt.imshow((100*(our_ff-god_ff)/god_ff),extent=(-FoV[0]/2,FoV[0]/2,-FoV[1]/2,FoV[1]/2), vmin = -0.5,vmax = 0.5, cmap='gray')
   plt.gca().set_xticklabels([])
   plt.gca().set_yticklabels([])
+  #plt.axis('equal')
   # Plot best-in-basis and fitted
   plt.subplot(223)
-  plt.xlabel(alpha)
-  plt.ylabel(beta)
+  plt.xlabel(xlab)
+  plt.ylabel(ylab)
   levels = np.arange(0.5,1.5,0.01)
   CS = plt.contour(bestfit_X,bestfit_Y,bestfit_ff,levels ,colors='k',alpha=0.3)
   CS2 = plt.contour(our_X, our_Y, our_ff, levels,colors='k')
   plt.clabel(CS2, inline=1)
   plt.xlim(-FoV[0]/2, FoV[0]/2)
   plt.ylim(-FoV[1]/2, FoV[1]/2)
-
+  #plt.axis('equal')
   # Plot residual between fitted and best-in-basis
   plt.subplot(224)
   a = plt.imshow((100*(our_ff-bestfit_ff) /bestfit_ff),extent=(-FoV[0]/2,FoV[0]/2,-FoV[1]/2,FoV[1]/2), vmin = -0.5,vmax = 0.5, cmap='gray')
-  plt.xlabel(alpha) 
-
+  plt.xlabel(xlab) 
+  #plt.axis('equal')
   plt.gca().set_yticklabels([])
   # Squeeze subplots together
   plt.subplots_adjust(wspace=0.0,hspace=0.0)
@@ -158,7 +162,6 @@ def camera_image(params, out_dir, camera_filename):
   plt.xlim(sky[0] - buffer_sky*dalpha_sky, sky[1] + buffer_sky*dalpha_sky)
   plt.ylim(sky[2] - buffer_sky*dbeta_sky, sky[3] + buffer_sky*dbeta_sky)
   
-
   # Plot sources on focal plane
   plt.subplot(122)
   plt.plot(x,y,'k.', markersize=6)
@@ -185,21 +188,21 @@ def camera_image(params, out_dir, camera_filename):
 
 def SurveyInnvar(params, survey_filemane, invvar_filename):
   print survey_filename
-  plt.figure(figsize = (fig_width, 0.5*fig_width))
+  plt.figure(figsize = (fig_width, 0.4*fig_width))
   plt.clf()
-  
   plt.subplot(121)
+  area = (params['sky_limits'][1]-params['sky_limits'][0])*(params['sky_limits'][3]-params['sky_limits'][2])
   survey_dic = pickle.load(open(survey_filename))
   mag = survey_dic['mag']
   all_mag = survey_dic['all_sources']
   fit_mag = survey_dic['fit_mag']
-  fit_dens = survey_dic['fit_dens']
+  fit_dens = survey_dic['fit_dens'] / (area)
   plt.semilogy(fit_mag, fit_dens, 'k:')
   hist_mag = np.arange(params['m_min'], params['m_max']+0.5, 0.5)
-  plt.bar(hist_mag[:-1], np.histogram(all_mag, bins = hist_mag)[0], width = 0.5, color = 'w')
-  plt.bar(hist_mag[:-1], np.histogram(mag, bins = hist_mag)[0], width = 0.5, hatch = '/', color = 'k', alpha =0.3)
+  plt.bar(hist_mag[:-1], np.histogram(all_mag, bins = hist_mag)[0] / (area) , width = 0.5, color = 'w')
+  plt.bar(hist_mag[:-1], np.histogram(mag, bins = hist_mag)[0] / (area) , width = 0.5, hatch = '/', color = 'k', alpha =0.3)
   plt.xlabel(r'Source Magnitude (AB)')
-  plt.ylabel(r'Sources within Survey ((0.5 mag)$^{-1})$')
+  plt.ylabel(r'Density of Sources ((0.5 mag)$^{-1})$')
   
   print invvar_filename
   plt.subplot(122)
@@ -214,7 +217,7 @@ def SurveyInnvar(params, survey_filemane, invvar_filename):
   sort = sort[sort[:,0].argsort(),:]
   sort_reported_invvar = sort[:,1]
   sort_counts = sort[:,0]
-  plt.plot(np.log10(counts), np.log10((1/true_invvar)/counts**2),'k.', markersize = 2., label = "Actual Variance", alpha = 0.1)
+  plt.plot(np.log10(counts), np.log10((1/true_invvar)/counts**2),'k.', markersize = 2., label = "Actual Variance", alpha = 0.01)
   plt.plot(np.log10(sort_counts), np.log10((1/sort_reported_invvar)/sort_counts**2),'k', label = "Assumed Variance")
   plt.xlabel(r'$\log_{10}(c_i)$')
   plt.ylabel(ur'$\log_{10}(\frac{{\sigma_i}^2}{c_i^2})$')
